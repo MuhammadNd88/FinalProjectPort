0c34b5388afc20e6958574d1b478f2903c6f4aed
MuhammadNd88 <46718678+MuhammadNd88@users.noreply.github.com>
2019-04-12 16:35:38 -0600

    main content worked

--- js/menu.js
+++ js/menu.js
@@ -1,43 +1,39 @@
-function init () {
-    var intro = [
-    {heading: "I am Muhammad Nadeem"},
-    {heading:"I am Web Developer"}, 
-    {heading:"I have a HTML CSS and JavaScript experience"}, 
-    {heading: "I have a graphic designer experince"}
-          ];
-    
-    var counter = 0;
+function init() {
+  var intro = [
+    { heading: "I am Muhammad Nadeem." },
+    { heading: "I am a Web Developer." },
+    { heading: "I do HTML, CSS and JavaScript." },
+    { heading: "I also have graphic design experience." }
+  ];
 
-    function showIntro() {
-        var chosenIntro = intro[counter];
-        document.querySelector('#introHeading').innerHTML = chosenIntro.heading;
-        
-    }
-    function chooseNextIntro() {
-        if (counter < intro.length -1) {
-            counter++;
-        } else {
-            counter = 0;
-        }
-        showIntro();
-    }
+  var counter = 0;
 
-    function choosePrevIntro() {
-        if (counter > 0) {
-            counter--;
-        } else {
-            counter = intro.length-1;
-        }
-        showIntro();
+  function showIntro() {
+    var chosenIntro = intro[counter];
+    document.querySelector("#introHeading").innerHTML = chosenIntro.heading;
+  }
+  function chooseNextIntro() {
+    if (counter < intro.length - 1) {
+      counter++;
+    } else {
+      counter = 0;
     }
-
-    
-    document.getElementById('next').addEventListener("click", chooseNextIntro);
-    document.getElementById('prev').addEventListener("click", choosePrevIntro);
     showIntro();
-    var quoteTimer = setInterval(chooseNextIntro, 3000);
+  }
+
+  function choosePrevIntro() {
+    if (counter > 0) {
+      counter--;
+    } else {
+      counter = intro.length - 1;
+    }
+    showIntro();
+  }
+
+  document.getElementById("next").addEventListener("click", chooseNextIntro);
+  document.getElementById("prev").addEventListener("click", choosePrevIntro);
+  showIntro();
+  var quoteTimer = setInterval(chooseNextIntro, 3000);
 }
-
-
 
 init();
